f68416adb1d75ee675347c50f48022902b7e3d21
Lloyd Harris <32348967+lh694159@users.noreply.github.com>
2020-05-15 14:45:47 -0400

    update

--- time_delta.py
+++ time_delta.py
@@ -12,3 +12,23 @@ yesterday = timedelta(days=1);
 
 yesterday = today - yesterday
 print('Yesterday was : ' + str( yesterday))
+
+#dentions wil use timedelta to go back one week. 
+one_week = timedelta(weeks=1)
+last_week = today - one_week
+print('Last week was : ' + str(last_week))
+
+#use day, month, year, hour, minute, second functions 
+#to display only part of the date. All these functions return intergers.
+#Convert them to strings before concatenating them to another string. 
+
+print ('Day: ' + str (today.day))
+print ('Month: ' + str (today.month))
+print ('Year: ' + str(today.year))
+print ('Today, the ' + str(today.day) + 'th ' + ' of the ' + str(today.month)  + ' of the month marks the ' + str(today.day) + 'th  day in  the year' + str(today.year) + ', I am learning Python')
+
+# Another example t odemonstrate the use of datetime delta
+
+birth_day = input ('When is your birtday (dd/mm/yyy?')
+birthday_date = datetime.strptime(birth_day, '%d/%m/%Y')
+print('Birthday: ' + str(birth_day))
