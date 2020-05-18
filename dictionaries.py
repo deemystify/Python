65040a27cc96583b44de632258dee4c2713fcfbc
Lloyd Harris <32348967+lh694159@users.noreply.github.com>
2020-05-17 20:39:59 -0400

    another update

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+################################################
+# Dictionaries are important when you need to use key value pairs
+# Refer to the following example
+
+person = {'first': 'Christopher'} #In this example, the key is first, and the value is Christopher
+person['last' ] =  'Harrison'
+print(person)
+print(person['first'])
